dab9e39e2299afc8dbc8e5f1db4e3c427e4a2d3c
XOLISANI NGCOBO <38984822+Xolsann722@users.noreply.github.com>
2021-02-21 18:27:38 +0200

    added more block if statements

--- guessingGame.py
+++ guessingGame.py
@@ -5,7 +5,36 @@ guess =int(input())
 
 if guess < answer:
     print("Please guess higher")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
 elif guess > answer:
     print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
 else:
     print("You got it first time")
+
+# exercise 1 If statement
+# tree1 = "pine"
+# tree2 = "gum"
+# if tree1 ==tree2:
+#     print("The trees are the same")
+# else:
+#     print("The trees are different      n  ")
+
+# exercise 2
+# x = 5
+# y = 7
+#
+# if x > y:
+#     print("x is greater than y")
+# elif x < y:
+#     print("x is smaller than y")
+# elif x == y:
+#     print("x equals y")
