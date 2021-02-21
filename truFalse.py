24fa4d597990a3a4e066f9494a6f1054823226be
XOLISANI NGCOBO <38984822+Xolsann722@users.noreply.github.com>
2021-02-21 20:06:10 +0200

    checking in and truFalse

--- truFalse.py
+++ truFalse.py
@@ -1,8 +1,18 @@
-day = "Saturday"
-temperature = 30
-raining = True
-
-if (day == "Saturday" and temperature ) > 27 or not raining:
-    print("Go swimming")
+if 0:
+    print("True")
 else:
-    print("Learn Python")
+    print("False")
+
+name = input("Please enter your name: ")
+if name != "":
+    print("Hello, {}".format(name))
+else:
+    print("are a man with no name? ")
+# day = "Saturday"
+# temperature = 30
+# raining = True
+#
+# if (day == "Saturday" and temperature ) > 27 or not raining:
+#     print("Go swimming")
+# else:
+#     print("Learn Python")
